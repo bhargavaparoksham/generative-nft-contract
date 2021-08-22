38da472819be673760e3400476e84a7396d3aeec
Bhargav Aparoksham <aparokshambhargav@gmail.com>
2021-08-22 22:14:39 +0530

    refactoring &  bug fixes

--- contracts/generativeNFT.sol
+++ contracts/generativeNFT.sol
@@ -28,12 +28,12 @@ contract generativeNFT is ERC721, Ownable, VRFConsumerBase {
     uint public maxPurchase;
     // Maxium number of lottery prize winners
     uint public maxWinners;
-    // Number of winners selected as of now
-    uint public winnersCount;
+    // Winners selected
+    bool public winnersSelected;
     // Number of winners disbursed
     uint public winnersDisbursed;
     // Array of nfts IDs that won the lottery.
-    uint[] public winnerIDs;
+    uint[] public winnerIDs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     // Array of prize money percent w.r.t total prize money pool that will be disbursed to each winner
     uint[] public prizeMoneyPercent = [40, 4, 2, 2, 2, 2, 2, 2, 2, 2];
     // Chainlink oracle fee
@@ -44,8 +44,10 @@ contract generativeNFT is ERC721, Ownable, VRFConsumerBase {
     address public LINKTokenAddress;
     // Chainlink VRF coordinator address
     address public ChainlinkVRFCoordinator;
-    // Chainlink Available in the contract
-    uint256 public chainlinkAvailable;
+    // Chainlink randomness value
+    uint256 public chainlinkRandomness;
+
+
 
 
     // ============ Events ============
@@ -54,8 +56,10 @@ contract generativeNFT is ERC721, Ownable, VRFConsumerBase {
     event SaleActive(bool saleIsActive);
     // Address of nft minter and number of nfts minted
     event NFTsMinted(address indexed minter, uint256 numNFTs);
-    // Chainlink VRF request ID, winner position & winner nft ID
-    event WinnerSelected(bytes32 id, uint winnerPosition, uint winnerID);
+    // Random Number
+    event RandomNumberGenerated(bytes32 indexed requestId, uint256 randomNum);
+    // Winners have been selected.
+    event WinnerSelected(bool winnersSelected);
 
     // ============ Constructor ============
 
@@ -69,20 +73,20 @@ contract generativeNFT is ERC721, Ownable, VRFConsumerBase {
         bool _saleIsActive,
         uint _maxPurchase,
         uint _maxWinners
-        ) ERC721 ("Generative NFT","GNFT") VRFConsumerBase(_ChainlinkVRFCoordinator, _LINKTokenAddress) {  
+        ) ERC721 ("Fairy Sparkles","FYS") VRFConsumerBase(_ChainlinkVRFCoordinator, _LINKTokenAddress) {  
         nftsMinted = 0;
         ChainlinkVRFCoordinator = _ChainlinkVRFCoordinator;
         LINKTokenAddress = _LINKTokenAddress;
+        ChainlinkKeyHash = _ChainlinkKeyHash;
+        ChainlinkFee = _ChainlinkFee;
         nftMintLimit = _nftMintLimit;
         mintPrice = _mintPrice; 
         saleIsActive = _saleIsActive;
         maxPurchase = _maxPurchase;
         maxWinners = _maxWinners;
-        winnersCount = 0;
+        winnersSelected = false;
         winnersDisbursed = 0;
-        ChainlinkKeyHash = _ChainlinkKeyHash;
-        ChainlinkFee = _ChainlinkFee;
-
+        chainlinkRandomness = 0;
     }
 
 
@@ -124,7 +128,7 @@ contract generativeNFT is ERC721, Ownable, VRFConsumerBase {
         // Require that number of nfts to be minted is less than or equal to max purchase allowed
         require(_numNFTs <= maxPurchase, "Generative Art: Can only mint 20 tokens at a time");
         // Require total nfts that will be minted is less than the nft limit.
-        require((_numNFTs + nftsMinted) < nftMintLimit, "Generative Art: Can't mint more than 10,000 NFTs");
+        require((_numNFTs + nftsMinted) <= nftMintLimit, "Generative Art: Can't mint more than 10,000 NFTs");
         // Require sufficient Eth is provided to mint nfts
         require(msg.value == (_numNFTs * mintPrice), "Generative Art: Insufficient ETH provided to mint NFTs.");
         // Mint the number of nfts requested.
@@ -167,49 +171,57 @@ contract generativeNFT is ERC721, Ownable, VRFConsumerBase {
     }
 
     /**
-    * Collects randomness from Chainlink VRF to propose winners.
+    * Get randomness from Chainlink VRF to propose winners.
     */
-    function collectRandomWinner() external returns (bytes32 requestId) {
+    function getRandomness() public returns (bytes32 requestId) {
         // Require at least 1 nft to be minted
         require(nftsMinted > 0, "Generative Art: No NFTs are minted yet.");
-        // Require caller to be contract owner or all 10K NFTs need to be minted.
+        //Require caller to be contract owner or all 10K NFTs need to be minted
         require(msg.sender == owner() || nftsMinted == nftMintLimit , "Generative Art: Only Owner can collectWinner. All 10k NFTs need to be minted for others to collectWinner.");
+        // Require Winners to be not yet selected
+        require(!winnersSelected, "Generative Art: Winners already selected");
         // Require chainlinkAvailable >= Chainlink VRF fee
-        require(chainlinkAvailable >= ChainlinkFee, "Generative Art: Insufficient LINK. Please deposit LINK.");
+        require(IERC20(LINKTokenAddress).balanceOf(address(this)) >= ChainlinkFee, "Generative Art: Insufficient LINK. Please deposit LINK.");
         // Call for random number
         return requestRandomness(ChainlinkKeyHash, ChainlinkFee);
     }
 
     /**
-    * Collects random numbers from Chainlink VRF
+    * Collect random number from Chainlink VRF
     */
     function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {        
-        //Update winner IDs with a random number between 0 & nftsMinted.
-        winnerIDs[winnersCount] = (randomness % nftsMinted);
-        // Increment winnersCount
-        winnersCount = winnersCount + 1;
-        // Update chainlink available
-        chainlinkAvailable = chainlinkAvailable - ChainlinkFee;
-        // Emit winner selected event
-        emit WinnerSelected(requestId, winnersCount, winnerIDs[winnersCount-1]);
-
+        //Store randomness
+        chainlinkRandomness = randomness;
+        emit RandomNumberGenerated(requestId, randomness);
     }
 
     /**
     * Withdraw Link from the contract.
     */
     function withdrawLink(address _to) public onlyOwner {
-        IERC20(LINKTokenAddress).transfer(_to, chainlinkAvailable);
+        IERC20(LINKTokenAddress).transfer(_to, IERC20(LINKTokenAddress).balanceOf(address(this)));
     }
 
     /**
-    * Deposit Link tp the contract.
+    * Get randomness from Chainlink VRF to propose winners.
     */
-    function depositLink(uint256 _depositAmount) public {
-        // Transfer LINK to contract
-        IERC20(LINKTokenAddress).transferFrom(msg.sender, address(this), _depositAmount);
-        // Update chainlink available
-        chainlinkAvailable = chainlinkAvailable + _depositAmount;
+    function selectRandomWinners() public {
+        // Require at least 1 nft to be minted
+        require(nftsMinted > 0, "Generative Art: No NFTs are minted yet.");
+        //Require caller to be contract owner or all 10K NFTs need to be minted.
+        require(msg.sender == owner() || nftsMinted == nftMintLimit , "Generative Art: Only Owner can collectWinner. All 10k NFTs need to be minted for others to collectWinner.");
+        //Require winners to be not yet selected
+        require(!winnersSelected, "Generative Art: Winners already selected");
+        //Require chainlinkRandomness to be fullfilled
+        require(chainlinkRandomness != 0, "Generative Art: Get chainlink randomness first");
+
+        for (uint i = 0; i < maxWinners; i++) {
+            // select winner from 0 to nftsMinted
+            winnerIDs[i] = (uint256(keccak256(abi.encode(chainlinkRandomness, i))) % nftsMinted);
+        }
+
+        winnersSelected = true;
+        emit WinnerSelected(winnersSelected);
     }
 
     /**
@@ -221,13 +233,15 @@ contract generativeNFT is ERC721, Ownable, VRFConsumerBase {
         // Require caller to be contract owner or all 10K NFTs need to be minted
         require(msg.sender == owner() || nftsMinted == nftMintLimit , "Generative Art: Only Owner can disburseWinner. All 10k NFTs need to be minted for others to disburseWinner.");
         // Require that all winners be selected first before disbursing
-        require(winnersCount >= maxWinners, "Generative Art: Please wait for Chainlink VRF to update the winners first.");    
+        require(winnersSelected, "Generative Art: Winners needs to be selected first");
+        //Get account balance
+        uint256 accountBalance = address(this).balance;   
         // While winners disbursed is less than total winners
         while (winnersDisbursed < maxWinners) {
             // Get winner
             address winner = ownerOf(winnerIDs[winnersDisbursed]);
             // Transfer Prize Money to winner
-            payable(winner).transfer(((address(this).balance)*prizeMoneyPercent[winnersDisbursed])/100);
+            payable(winner).transfer(accountBalance*((prizeMoneyPercent[winnersDisbursed])/100));
             // Increment winnersDisbursed
             winnersDisbursed++;
         }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,42 @@
 const generativeNFT = artifacts.require("generativeNFT");
+var ChainlinkVRFCoordinator = '0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B';
+var LINKTokenAddress = '0x01BE23585060835E02B77ef475b0Cc51aA1e0709';
+var ChainlinkKeyHash = '0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311';
+var ChainlinkFee = '100000000000000000'; // 0.1 LINK on Rinkeby
+var nftMintLimit = 10;
+var mintPrice = '25000000000000000'; //wei or 0.025 eth 25000000000000000
+var saleIsActive = false;
+var maxPurchase = 3;
+var maxWinners = 2;
 
+/* baseURI = 'ipfs://QmVAqWSPcCK6evkd1yyxjCpWsNdFMiKTp8vNxvYWUREsdq/' */
 
+/**
+* 
+* Network: Rinkeby
+* Chainlink VRF Coordinator address: 0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B
+* LINK token address: 0x01BE23585060835E02B77ef475b0Cc51aA1e0709
+* Key Hash: 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311
+*
+**/
 
 module.exports = async function(deployer, network, accounts) {
 
 
   //Deploy generativeNFT
 
-  await deployer.deploy(generativeNFT)
+  await deployer.deploy(
+  	generativeNFT,
+  	ChainlinkVRFCoordinator, 
+  	LINKTokenAddress, 
+  	ChainlinkKeyHash, 
+  	ChainlinkFee,
+  	nftMintLimit,
+  	mintPrice,
+  	saleIsActive,
+  	maxPurchase,
+  	maxWinners
+  	)
   const genNFT = await generativeNFT.deployed()
 
 
